a2aeb013bc371b7e49ebae5de7dbe78f3fd26165
lulcca <lucca.jacomassi@hotmail.com>
2025-11-16 01:26:44 -0300

    :alembic: telemetry example

---
+++ layers/base/server/middleware/logger.ts
@@ -0,0 +1,9 @@
+export default defineEventHandler(async (_event) => {
+  // const storage = useStorage('db');
+
+  // await storage.setItem(`telemetry:request:${Date.now()}`, {
+  //   headers: getRequestHeaders(event),
+  //   method: event.method,
+  //   url: getRequestURL(event),
+  // });
+});
