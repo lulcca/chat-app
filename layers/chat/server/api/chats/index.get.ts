60ecd7b82c6e7cfe802140011a0f9aa5e8766b88
lulcca <lucca.jacomassi@hotmail.com>
2025-11-16 01:28:56 -0300

    :zap: optimize API with cached handler

--- layers/chat/server/api/chats/index.get.ts
+++ layers/chat/server/api/chats/index.get.ts
@@ -1,6 +1,21 @@
 import { getAllChats } from '~~/layers/chat/server/repository/chat-repository';
 
-export default defineEventHandler(async (_event) => {
+export default defineCachedEventHandler(async (_event) => {
+  const storage = useStorage('db');
+
+  await storage.setItem('chats:has-new-chat', false);
+
   return getAllChats();
+}, {
+  maxAge: 0,
+  name: 'getAllChats',
+  async shouldInvalidateCache() {
+    const storage = useStorage('db');
+
+    const has_new_chat = await storage.getItem<boolean>('chats:has-new-chat');
+
+    return has_new_chat || false;
+  },
+  swr: false,
 });
 
--- layers/chat/server/api/chats/index.post.ts
+++ layers/chat/server/api/chats/index.post.ts
@@ -3,5 +3,9 @@ import { createChat } from '~~/layers/chat/server/repository/chat-repository';
 export default defineEventHandler(async (event) => {
   const { projectId, title } = await readBody(event);
 
+  const storage = useStorage('db');
+
+  await storage.setItem('chats:has-new-chat', true);
+
   return createChat({ projectId, title });
 });
